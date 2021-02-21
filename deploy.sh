c8bb8f72b7c5ca74733298faf84dede8e03ef8ae
Jeff Palmer <palmer1026@gmail.com>
2021-02-21 08:30:11 -0600

    Fix typo in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t jpalmer1026/multi-client:latest -t jpalmer1026/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t jpalmer1026/multi-server:latest jpalmer1026/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jpalmer1026/multi-server:latest -t jpalmer1026/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t jpalmer1026/multi-worker:latest -t jpalmer1026/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push jpalmer1026/multi-client:latest
